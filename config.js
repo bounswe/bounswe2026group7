b065c876f40ad7b056f42a324b9f78af1fc59526
burakogut5 <ogutburak5@gmail.com>
2026-03-12 14:25:39 +0300

    feat: add Burak button with random dog photo API

--- config.js
+++ config.js
@@ -4,12 +4,13 @@
  * Change NUM_BUTTONS to control how many buttons appear on the homepage.
  * Update BUTTON_LABELS to set the text displayed on each button.
  */
-const NUM_BUTTONS = 6;
+const NUM_BUTTONS = 7;
 const BUTTON_LABELS = [
   "Button 1",
   "Button 2",
   "Button 3",
   "Button 4",
   "Button 5",
-  "Burak Ögüt",
+  "Random Dog Photo API",
+  "Button 7",
 ];
